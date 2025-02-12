9ee8fc3403f8509fb41c31e0f326f836d6b5a8c4
Jesin <Jesin00@gmail.com>
2025-02-12 15:33:24 -0800

    switch errors on duplicate tags

--- base-env.lua
+++ base-env.lua
@@ -261,6 +261,9 @@ local function switch_impl(syntax, env)
 		end
 		--TODO rewrite this to collect the branch envs and join them back together:
 		tag, term = table.unpack(tag)
+		if variants:get(tag.str) ~= nil then
+			return false, "duplicate case tag " .. tag.str
+		end
 		variants:set(tag.str, term)
 		variant_debug:set(tag.str, var_debug(tag.str, tag.start_anchor))
 	end
